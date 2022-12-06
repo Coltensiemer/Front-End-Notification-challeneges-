19e770dece7633cba268968167fcfb82ddfb10e1
Colten Siemergit <colten50@hotmail.com>
2022-12-06 00:15:16 -0500

    update

--- js.js
+++ js.js
@@ -1,32 +1,37 @@
 'use strict'
 
-// const profileElement = document.getElementById("profile-element"); 
-const profile = document.querySelectorAll(".profile-container"); 
-const removeAllBtn = document.querySelector('.remove-all-btn');
-const dot= document.querySelectorAll(".span-dot") ;
 
 
-
-
-
-
-
-// When you click on profile 
-
-profile.forEach(profiles => { 
-    profiles.addEventListener('click', ()=> { 
-      profiles.classList.remove('active')
-    })
-    }); 
-
+(function(){ // const profileElement = document.getElementById("profile-element"); 
+    const profile = document.querySelectorAll(".profile-container"); 
+    const removeAllBtn = document.querySelector('.remove-all-btn');
+    const dot= document.querySelectorAll(".span-dot") ;
     
-
-//Mark all as read 
-
-removeAllBtn.addEventListener('click', ()=> { 
+    
+    
+    
+    
+    
+    
+    // When you click on profile 
+    
     profile.forEach(profiles => { 
-        profiles.classList.remove('active')
-       
+        profiles.addEventListener('click', ()=> { 
+          profiles.classList.remove('active')
+        })
+        }); 
+    
+        
+    
+    //Mark all as read 
+    
+    removeAllBtn.addEventListener('click', ()=> { 
+        profile.forEach(profiles => { 
+            profiles.classList.remove('active')
+           
+        })
     })
-})
+    })()
+
+
 
